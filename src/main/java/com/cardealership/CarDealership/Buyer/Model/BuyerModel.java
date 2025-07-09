b5a00d28659305bc8b6bf8711e3f10f15f6d716d
heitorfonsatti <125671020+heitorfonsatti@users.noreply.github.com>
2025-07-09 16:26:18 -0300

    Classe Model desenvolvida.

--- src/main/java/com/cardealership/CarDealership/Buyer/Model/BuyerModel.java
+++ src/main/java/com/cardealership/CarDealership/Buyer/Model/BuyerModel.java
@@ -1,4 +1,38 @@
 package com.cardealership.CarDealership.Buyer.Model;
 
-public class BuyerModel {
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.hateoas.RepresentationModel;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_buyer")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class BuyerModel extends RepresentationModel<BuyerModel> implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    //Atributos
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID idBuyer;
+    private String name;
+    private String location;
+    private String email;
+    private LocalDate dateOfBirth;
+
+    @Transient
+    public int getAge() {
+        return LocalDate.now().getYear() - this.getDateOfBirth().getYear();
+    }
 }
