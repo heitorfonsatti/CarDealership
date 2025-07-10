ff5927d4af66f2c0e3d754f8526a77219360670b
heitorfonsatti <125671020+heitorfonsatti@users.noreply.github.com>
2025-07-10 11:44:36 -0300

    Classe Controller desenvolvida.

--- src/main/java/com/cardealership/CarDealership/Buyer/Controller/BuyerController.java
+++ src/main/java/com/cardealership/CarDealership/Buyer/Controller/BuyerController.java
@@ -1,4 +1,80 @@
 package com.cardealership.CarDealership.Buyer.Controller;
 
+import com.cardealership.CarDealership.Buyer.DTO.BuyerRecordDTO;
+import com.cardealership.CarDealership.Buyer.Model.BuyerModel;
+import com.cardealership.CarDealership.Buyer.Repository.BuyerRepository;
+import jakarta.validation.Valid;
+import org.apache.coyote.Response;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@RestController
 public class BuyerController {
+
+    @Autowired
+    BuyerRepository buyerRepository;
+
+    @PostMapping("/buyer")
+    public ResponseEntity<BuyerModel> saveBuyer(@RequestBody @Valid BuyerRecordDTO buyerRecordDTO) {
+        var buyerModel = new BuyerModel();
+        BeanUtils.copyProperties(buyerRecordDTO, buyerModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(buyerRepository.save(buyerModel));
+    }
+
+    //Get all
+    @GetMapping("/buyer")
+    public ResponseEntity<List<BuyerModel>> getAllBuyers() {
+        List<BuyerModel> buyerList = buyerRepository.findAll();
+        if (!buyerList.isEmpty()) {
+            for (BuyerModel buyer : buyerList) {
+                UUID id = buyer.getIdBuyer();
+                buyer.add(linkTo(methodOn(BuyerController.class).getOneBuyer(id)).withSelfRel());
+            }
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(buyerList);
+    }
+
+    //Get one
+    @GetMapping("/buyer/{id}")
+    public ResponseEntity<Object> getOneBuyer(@PathVariable(value="id") UUID id) {
+        Optional<BuyerModel> buyer0 = buyerRepository.findById(id);
+        if (buyer0.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Buyer not found");
+        }
+        buyer0.get().add(linkTo(methodOn(BuyerController.class).getAllBuyers()).withRel("Buyer List"));
+        return ResponseEntity.status(HttpStatus.OK).body(buyer0.get());
+    }
+
+    //Update
+    @PutMapping("/buyer/{id}")
+    public ResponseEntity<Object> updateBuyer(@PathVariable(value="id") UUID id, @RequestBody @Valid BuyerRecordDTO buyerRecordDTO) {
+        Optional<BuyerModel> buyer0 = buyerRepository.findById(id);
+        if (buyer0.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Buyer not found");
+        }
+        var buyerModel = buyer0.get();
+        BeanUtils.copyProperties(buyerRecordDTO, buyerModel);
+        return ResponseEntity.status(HttpStatus.OK).body(buyerRepository.save(buyerModel));
+    }
+
+    //Delete
+    @DeleteMapping("/buyer/{id}")
+    public ResponseEntity<Object> deleteBuyer(@PathVariable(value="id") UUID id) {
+        Optional<BuyerModel> buyer0 = buyerRepository.findById(id);
+        if (buyer0.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Buyer not found");
+        }
+        buyerRepository.delete(buyer0.get());
+        return ResponseEntity.status(HttpStatus.OK).body("Buyer Deleted Successfully");
+    }
 }
--- src/main/java/com/cardealership/CarDealership/Buyer/Repository/BuyerRepository.java
+++ src/main/java/com/cardealership/CarDealership/Buyer/Repository/BuyerRepository.java
@@ -1,4 +1,11 @@
 package com.cardealership.CarDealership.Buyer.Repository;
 
-public interface BuyerRepository {
+import com.cardealership.CarDealership.Buyer.Model.BuyerModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface BuyerRepository extends JpaRepository<BuyerModel, UUID>{
 }
