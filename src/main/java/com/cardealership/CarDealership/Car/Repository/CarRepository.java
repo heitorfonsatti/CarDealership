d90bc85cd996b2931ff369380b6e464d71f405f8
heitorfonsatti <125671020+heitorfonsatti@users.noreply.github.com>
2025-07-15 15:49:40 -0300

    Classe de repositório desenvolvida.

--- src/main/java/com/cardealership/CarDealership/Car/Repository/CarRepository.java
+++ src/main/java/com/cardealership/CarDealership/Car/Repository/CarRepository.java
@@ -1,4 +1,9 @@
 package com.cardealership.CarDealership.Car.Repository;
 
-public interface CarRepository {
+import com.cardealership.CarDealership.Car.Model.CarModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface CarRepository extends JpaRepository<CarModel, UUID> {
 }
